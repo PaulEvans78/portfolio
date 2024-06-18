72f1a7e3bd40bf728df5454997916e67c93582df
Paul Evans <paulevansdop@gmail.com>
2024-06-18 11:53:56 +0200

    button animation

--- src/components/ButtonPlayVikings.jsx
+++ src/components/ButtonPlayVikings.jsx
@@ -1,66 +1,9 @@
-// import React from "react";
-// import styled from "styled-components";
-// import { FaPlayCircle } from "react-icons/fa";
-
-// const StyledButtonContainer = styled.div`
-//   width: 200px;
-//   display: flex;
-//   flex-direction: column;
-//   align-items: flex-start;
-//   padding-top: 30px;
-//   color: var(--main-button-color);
-
-//   @media screen and (max-width: 478px) {
-//     align-items: center;
-//   }
-// `;
-
-// const StyledWrapper = styled.div`
-//   display: inline-block;
-//   transition: transform 0.3s ease, color 0.3s ease;
-//   &:hover {
-//     transform: scale(1.1);
-//     color: var(--main-button-hover-color);
-//   }
-
-//   @media (max-width: 960px) {
-//     &:hover {
-//       transform: none;
-//       color: inherit;
-//     }
-//   }
-// `;
-
-// const Styledp = styled.p`
-//   color: var(--main-font-color);
-//   font-size: 20px;
-//   font-weight: 600;
-
-//   @media (max-width: 960px) {
-//     font-size: 18px;
-//   }
-// `;
-
-// const Button = ({ onClick }) => {
-//   const handleClick = () => {
-//     onClick();
-//   };
-
-//   return (
-//     <StyledButtonContainer>
-//       <StyledWrapper>
-//         <FaPlayCircle onClick={onClick} size={70} />
-//       </StyledWrapper>
-//       <Styledp>Play Trailer</Styledp>
-//     </StyledButtonContainer>
-//   );
-// };
-// export default Button;
-
-
-import React, { useState } from "react";
-import styled, { keyframes } from "styled-components";
+import React from "react";
+import styled from "styled-components";
 import { FaPlayCircle } from "react-icons/fa";
+
+
+
 
 const StyledButtonContainer = styled.div`
   width: 200px;
@@ -75,8 +18,23 @@ const StyledButtonContainer = styled.div`
   }
 `;
 
-const loadingAnimation = keyframes`
-  0% {
+const StyledWrapper = styled.div`
+  display: inline-block;
+  transition: transform 0.3s ease, color 0.3s ease;
+  
+  &:hover {
+    transform: scale(1.1);
+    color: var(--main-button-hover-color);
+  }
+
+  &:active {
+    border-radius: 50%;
+    border: solid 2px white;
+    animation: pulse 0.5s ease;
+  }
+
+  @keyframes pulse {
+    0% {
     border-color: rgba(255, 255, 255, 0.3);
   }
   50% {
@@ -85,22 +43,7 @@ const loadingAnimation = keyframes`
   100% {
     border-color: rgba(255, 255, 255, 0.3);
   }
-`;
-
-const StyledWrapper = styled.div`
-  display: inline-block;
-  transition: transform 0.3s ease, color 0.3s ease;
-  border: 2px solid transparent; /* Initial border, hidden */
-  ${({ isLoading }) =>
-    isLoading &&
-    `
-    border: 2px solid rgba(255, 255, 255, 0.3);
-    animation: ${loadingAnimation} 1s infinite;
-  `}
-  &:hover {
-    transform: scale(1.1);
-    color: var(--main-button-hover-color);
-  }
+}
 
   @media (max-width: 960px) {
     &:hover {
@@ -121,22 +64,18 @@ const Styledp = styled.p`
 `;
 
 const Button = ({ onClick }) => {
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handleClick = async () => {
-    setIsLoading(true);
-    await onClick();
-    setIsLoading(false);
+  const handleClick = () => {
+    onClick();
   };
 
   return (
     <StyledButtonContainer>
-      <StyledWrapper isLoading={isLoading}>
-        <FaPlayCircle onClick={handleClick} size={70} />
+      <StyledWrapper>
+        <FaPlayCircle onClick={onClick} size={70} />
       </StyledWrapper>
       <Styledp>Play Trailer</Styledp>
     </StyledButtonContainer>
   );
 };
-
 export default Button;
+
